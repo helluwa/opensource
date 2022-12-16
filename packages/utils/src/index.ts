895e8d061938dc59b5425fbd40a3fe7be7e080b4
Karthikeyan Mariappan <karthikeyanmariappan@hotmail.com>
2022-12-16 11:17:44 +0100

    refactor(utils): :recycle: functions cleanup

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,11 +1,3 @@
 import UUID from "./lib/uuid";
 
 export { UUID }
-
-export const add = (a: number, b: number): number => {
-    return a + b
-}
-
-export const multiple = (a:number, b:number): number => {
-    return a * b
-} 
