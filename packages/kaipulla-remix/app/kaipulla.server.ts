71d65775e4429cdd6c8e0741c682bd96921d77c0
Karthikeyan Mariappan <karthikeyanmariappan@hotmail.com>
2022-12-21 19:03:14 +0100

    remix environment setup

--- packages/kaipulla-remix/app/kaipulla.server.ts
+++ packages/kaipulla-remix/app/kaipulla.server.ts
@@ -1,10 +1,8 @@
 import { KaipullaCore, createUpstashProvider } from '@helluwa/kaipulla-core'
 import { Redis } from '@upstash/redis/with-fetch'
 
-const kaipullaProto = {
-  url: "https://eu2-closing-skunk-30334.upstash.io",
-  token: "AXZ-ACQgMWY5ZGFiZGEtMTc0YS00ZGVlLTlmODYtOThiNmQ3NTFhNTUzZDY4OWEyZjUzNTg2NGY0Y2I3ODZhYjI3Y2UzZDMxZGU="
-}
+const encryption_key = process.env.ENCRYPTION_SECRET_KEY as string
+const url = process.env.UPSTASH_REDIS_URL as string
+const token = process.env.UPSTASH_REDIS_TOKEN as string
 
-const ENCRYPTION_SECRET_KEY = 'yupl1za+WlirGjyNCWna6ZKnW1rDsZ/0YPLSwJjmmT0='
-export const kaipulla = new KaipullaCore({ db: createUpstashProvider(new Redis({ ...kaipullaProto })), encryption_key: ENCRYPTION_SECRET_KEY }) 
+export const kaipulla = new KaipullaCore({ db: createUpstashProvider(new Redis({ url, token })), encryption_key }) 
